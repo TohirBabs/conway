ae036aaabaab622887c18cdcc860a2c202034b7b
dev_bro <tohirbabs@gmail.com>
2022-03-25 14:34:31 +0100

    updates

--- src/index.js
+++ src/index.js
@@ -1,17 +1,120 @@
-import React from 'react';
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import App from './App';
+class Box extends Component {
+  selectBox= () => {
+    this.props.selectBox(this.props.row, this.props.col)
+  }
+
+  render() {
+    return (
+      <div className={this.props.boxClass}
+        id={this.props.id}
+        onClick={this.selectBox}
+      />
+    )
+  }
+}
+
+
+
+class Grid extends Component {
+  render() {
+    const width = this.props.cols * 16;
+    var rowsArr = [];
+    
+    var boxClass = '';
+    for (let i = 0; i < this.props.rows; i++) {
+      for (let j = 0; j < this.props.cols; j++){
+        let boxId= i + '_' + j;
+
+        boxClass = this.props.gridFull[i][j] ? 'box on' : 'box off'
+        rowsArr.push(
+          <Box 
+            boxClass={boxClass}
+            key={boxId}
+            boxId={boxId}
+            row={i}
+            col={j}
+            selectBox={this.props.selectBox}
+          />
+        );
+      }
+      
+    }
+
+    return (
+      <div className='grid' style={{width:width}}> 
+        {rowsArr}
+      </div>
+    )
+  }
+}
+
+
+class Main extends Component {
+  constructor(){
+    super();
+    this.speed = 100;
+    this.rows = 30;
+    this.cols = 50;
+
+    this.state = {
+      generation: 0,
+      gridFull: Array(this.rows).fill().map(() => Array(this.cols).fill(false))
+    }
+  }
+
+  selectBox = (row, col) => {
+    let gridCopy = arrayClone(this.state.gridFull);
+    gridCopy[row][col] = !gridCopy[row][col];
+    this.setState({
+      gridFull:gridCopy
+    })
+  }
+
+  seed = () => {
+    let gridCopy = arrayClone(this.state.gridFull);
+    
+    for (let i = 0; i < this.props.rows; i++) {
+      for (let j = 0; j < this.props.cols; j++){
+        if (Math.floor(Math.random() * 4)=== 1){
+          gridCopy[i][j] = true;
+        }
+      }
+    }
+    this.setState({
+      gridFull:gridCopy
+    })
+  }
+
+  componentDidMount() {
+    this.seed();
+  }
+
+  render() {
+    return(
+      <div>
+      <h1>The Game of Life</h1>
+      <Grid 
+        gridFull={this.state.gridFull}
+        rows={this.rows}
+        cols={this.cols}
+        selectBox={this.selectBox}
+      />
+      <h2>Generations: {this.state.generation}</h2>
+    </div>
+    )  
+  }
+}
+
+function arrayClone(arr) {
+  return JSON.parse(JSON.stringify(arr));
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Main />,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
